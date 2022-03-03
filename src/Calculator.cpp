abbf0cf1fc4bf75f7ff18725b6748a8b14e3401e
Tali Kalev <talkalev@gmail.com>
2022-03-03 19:11:41 +0200

    added list of commands from help command

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -86,12 +86,27 @@ void Calculator::print() const
 
 void Calculator::printErrorMsg() const
 {
-	std::cout << "NO!\n";
+	std::cout << "Command not found\n";
 }
 
 //-----------------------------------------
 
 void Calculator::printHelp() const
 {
-	std::cout << "help\n";
+	std::cout << "The available commands are:\n"
+			<< "* eval(uate) num ... - compute the result of the function #num on the following\n"
+			<< "set(s); each set is prefixed with the count of numbers to read\n"
+			<< "* uni(on) num1 num2 - creates an operation that is the union of operation\n"
+			<< "#num1 and operation #num2\n"
+			<< "* inter(section) num1 num2 - creates an operation that is the intersection\n"
+			<< "of operation #num1 and operation #num2\n"
+			<< "* diff(erence) num1 num2 - creates an operation that is the difference of\n"
+			<< "operation #num1 and operation #num2\n"
+			<< "* prod(uct) num1 num2 - creates an operation that returns the product of\n"
+			<< "the items from the results of operation #num1 and operation #num2\n"
+			<< "* comp(osite) num1 num2 - creates an operation that is the composition of\n"
+			<< "operation #num1 and operation #num2\n"
+			<< "* del(ete) num - delete operation #num from the operation list\n"
+			<< "* help - print this command list\n"
+			<< "* exit - exit the program\n";
 }
