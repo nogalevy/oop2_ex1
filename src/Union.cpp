c16f7993ccb6e108263591531d4b9dd17c092a44
Tali Kalev <talkalev@gmail.com>
2022-03-06 16:30:31 +0200

    added ranges to union:

--- src/Union.cpp
+++ src/Union.cpp
@@ -8,11 +8,7 @@ Union::Union(Set set1, Set set2)
 	:m_mySet({}) //const
 {
 	auto result = std::vector<int>();
-	//std::ranges::set_union(set1.getSet(), set2.getSet(), std::back_inserter(result));
-
-	std::vector<int>::iterator it;
-	int n = sizeof(sizeof(set1.getSet()) / sizeof(set1.getSet()[0]));
-
+	std::ranges::set_union(set1.getSet(), set2.getSet(), std::back_inserter(result));
 
 }
 
