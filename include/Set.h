941c8108da84a5e8402bedd84c682f6b5b4fc9fb
Tali Kalev <talkalev@gmail.com>
2022-03-13 13:17:12 +0200

    syntax changes:

--- include/Set.h
+++ include/Set.h
@@ -1,23 +1,14 @@
 #pragma once
 #include <iostream>
-//#include <stdlib.h>
 #include <vector>
-//#include <ranges>
 #include <algorithm>
 #include<iterator>
-#include<functional>
-#include <array>
-#include <ranges>
-#include <memory>
-
 
 class Set
 {
 public:
-	//Set() = default; //Tali : need a default constructor (for other classes) want to know if we should have default m_setValues = {0} or something -
-	//Noga: if we need default constructor so the default set is {} - empty set.
 	Set(std::vector<int> val);
-	void makeSet();
+	void makeSet(); //Tali: private?
 	std::vector<int> getSet()const;
 	void printSet()const;
 
